053aa450253475f835f51f7cef0c9f12f92824ac
Sammy Kumar <sammy.kumar267@gmail.com>
2015-06-04 19:24:53 -0400

    added biweekly and weekly logic and forms

--- salaryConverter.js
+++ salaryConverter.js
@@ -4,14 +4,17 @@ var app = angular.module('salaryApp', []);
 
 app.controller('converter', function($scope) {
   $scope.yearly = 80000;
-  console.log("log1");
+  //console.log("log1");
   $scope.monthly = $scope.yearly / 12;
-  console.log("log2");
+  $scope.biweekly = $scope.monthly / 2;
+  $scope.weekly = $scope.monthly / 4;
+ //console.log("log2");
   
   $scope.reCalculate = function(){
 		console.log("function was run");
 		$scope.monthly = $scope.yearly /12.00;
-    
+		$scope.biweekly = $scope.monthly / 2;
+		$scope.weekly = $scope.monthly / 4;
   }
 
 });
