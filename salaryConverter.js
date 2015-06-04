770ac0b40fdd8c8de9c22c58b7b94bf04c40e739
Sammy Kumar <sammy.kumar267@gmail.com>
2015-06-03 21:16:20 -0400

    defaults work

--- salaryConverter.js
+++ salaryConverter.js
@@ -5,14 +5,12 @@ var app = angular.module('salaryApp', []);
 app.controller('converter', function($scope) {
   $scope.yearly = 80000;
   $scope.monthly = $scope.yearly /12.00;
+  
+
 
 });
 
-scope.$watch('$scope.yearly', function() {
-       alert('hey, myVar has changed!');
-   });
-
-function reCalculate() {	      
-	 alert('hey, myVar has changed!');
-	// $scope.monthly = $scope.yearly /12.00;
+function reCalculate(yearly) {	      
+	$scope.monthly = yearly /12.00;
 }
+
